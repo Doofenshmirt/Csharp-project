ff5bf19c18e10113991a8ae168101d5c1e34e1ed
Doofenshmirt <103652691+Doofenshmirt@users.noreply.github.com>
2024-03-15 17:07:25 +0300

    Konsol Uygulamam
    Kendimi Geliştirmek Amaçlı yaptığım Projeler.

---
+++ Konsol_uygulamasi/Konsol_uygulamasi/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konsol_uygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("merhaba dünya");
+            Console.ReadKey();
+        }
+     
+    }
+}
