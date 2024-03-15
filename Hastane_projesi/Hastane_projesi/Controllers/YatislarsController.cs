46b4d552372eea1a97117319df60dc00bdaa0efa
Doofenshmirt <103652691+Doofenshmirt@users.noreply.github.com>
2024-03-15 17:08:48 +0300

    Hastane Projem
    Kendimi Geliştirmek Amaçlı yaptığım Projeler.

---
+++ Hastane_projesi/Hastane_projesi/Controllers/YatislarsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Hastane_projesi.Models;
+
+namespace Hastane_projesi.Controllers
+{
+    public class YatislarsController : Controller
+    {
+        private hastaneEntities db = new hastaneEntities();
+
+        // GET: Yatislars
+        public async Task<ActionResult> Index()
+        {
+            var yatislar = db.Yatislar.Include(y => y.Departmanlar).Include(y => y.Doktorlar).Include(y => y.hastalar);
+            return View(await yatislar.ToListAsync());
+        }
+
+        // GET: Yatislars/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yatislar yatislar = await db.Yatislar.FindAsync(id);
+            if (yatislar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yatislar);
+        }
+
+        // GET: Yatislars/Create
+        public ActionResult Create()
+        {
+            ViewBag.dept_no = new SelectList(db.Departmanlar, "dept_no", "dept_adi");
+            ViewBag.doktor_no = new SelectList(db.Doktorlar, "doktor_no", "doktor_adi");
+            ViewBag.hasta_no = new SelectList(db.hastalar, "hasta_no", "hasta_adi");
+            return View();
+        }
+
+        // POST: Yatislars/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "yatis_tarihi,cikis_tarihi,hasta_no,doktor_no,oda_no,dept_no,kayit_no")] Yatislar yatislar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Yatislar.Add(yatislar);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.dept_no = new SelectList(db.Departmanlar, "dept_no", "dept_adi", yatislar.dept_no);
+            ViewBag.doktor_no = new SelectList(db.Doktorlar, "doktor_no", "doktor_adi", yatislar.doktor_no);
+            ViewBag.hasta_no = new SelectList(db.hastalar, "hasta_no", "hasta_adi", yatislar.hasta_no);
+            return View(yatislar);
+        }
+
+        // GET: Yatislars/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yatislar yatislar = await db.Yatislar.FindAsync(id);
+            if (yatislar == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.dept_no = new SelectList(db.Departmanlar, "dept_no", "dept_adi", yatislar.dept_no);
+            ViewBag.doktor_no = new SelectList(db.Doktorlar, "doktor_no", "doktor_adi", yatislar.doktor_no);
+            ViewBag.hasta_no = new SelectList(db.hastalar, "hasta_no", "hasta_adi", yatislar.hasta_no);
+            return View(yatislar);
+        }
+
+        // POST: Yatislars/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "yatis_tarihi,cikis_tarihi,hasta_no,doktor_no,oda_no,dept_no,kayit_no")] Yatislar yatislar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(yatislar).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.dept_no = new SelectList(db.Departmanlar, "dept_no", "dept_adi", yatislar.dept_no);
+            ViewBag.doktor_no = new SelectList(db.Doktorlar, "doktor_no", "doktor_adi", yatislar.doktor_no);
+            ViewBag.hasta_no = new SelectList(db.hastalar, "hasta_no", "hasta_adi", yatislar.hasta_no);
+            return View(yatislar);
+        }
+
+        // GET: Yatislars/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Yatislar yatislar = await db.Yatislar.FindAsync(id);
+            if (yatislar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yatislar);
+        }
+
+        // POST: Yatislars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Yatislar yatislar = await db.Yatislar.FindAsync(id);
+            db.Yatislar.Remove(yatislar);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Hastane_projesi/Hastane_projesi/Controllers/hastalarsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Hastane_projesi.Models;
+
+namespace Hastane_projesi.Controllers
+{
+    public class hastalarsController : Controller
+    {
+        private hastaneEntities db = new hastaneEntities();
+
+        // GET: hastalars
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.hastalar.ToListAsync());
+        }
+
+        // GET: hastalars/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            hastalar hastalar = await db.hastalar.FindAsync(id);
+            if (hastalar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hastalar);
+        }
+
+        // GET: hastalars/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: hastalars/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "hasta_no,hasta_tc,hasta_adi,hasta_adres,hasta_cinsiyet")] hastalar hastalar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.hastalar.Add(hastalar);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(hastalar);
+        }
+
+        // GET: hastalars/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            hastalar hastalar = await db.hastalar.FindAsync(id);
+            if (hastalar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hastalar);
+        }
+
+        // POST: hastalars/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "hasta_no,hasta_tc,hasta_adi,hasta_adres,hasta_cinsiyet")] hastalar hastalar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hastalar).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(hastalar);
+        }
+
+        // GET: hastalars/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            hastalar hastalar = await db.hastalar.FindAsync(id);
+            if (hastalar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hastalar);
+        }
+
+        // POST: hastalars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            hastalar hastalar = await db.hastalar.FindAsync(id);
+            db.hastalar.Remove(hastalar);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
